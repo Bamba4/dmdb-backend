ef2db1150800251c64ad31bb837b999b52141629
Bamba <bamba4>
2021-01-25 00:06:26 +0000

    resolve runtime

--- settings.py
+++ settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
@@ -132,11 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
+
 GRAPHENE = {
   "SCHEMA": "schema",
   'MIDDLEWARE': [
